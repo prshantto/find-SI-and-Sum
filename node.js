e7de2f70397b60fedb11933d6316b3a7827b0d06
Prashant <prashanttt001@gmail.com>
2024-11-19 12:44:40 +0530

    added landing page route

--- node.js
+++ node.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const   app = express();
 
+app.get("/", (req,res) => {
+    res.send("hello there!")
+})
+
 
 app.get("/sum", (req,res) => {
     let a = Number(req.query.a);
